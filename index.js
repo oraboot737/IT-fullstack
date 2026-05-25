3a0a889353f1295ba3897397cf2e39a422b6f359
oraboot737 <oraboot.w@itm.kmutnb.ac.th>
2026-05-25 11:28:41 +0700

    add

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+require("dotenv").config();
+
+const express = require("express");
+const { networkInterfaces } = require("os");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
